ebb2902a4bfc993c08e1986f732e2af32677c268
SawyerPorte <sporte@ucsc.edu>
2025-09-30 16:29:11 -0700

    mine

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ export default function App() {
                 Global Stats Dashboard
             </h1>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 w-full">
+            <div class="grid grid-cols-4 gap-4">
                 {statCards.map((stat) => (
                     <div
                         key={stat.title}
